c3877b52d972766b12ec5b6b11e1a11d0f45c42a
willjschultz <willjschultz@gmail.com>
2021-04-07 22:54:10 -0400

    Refactor current routes to use Resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get "/events", to: "events#index"
-  get "/events/:id", to: "events#show"
   root "events#index"
+  
+  resources :events
   
 end
