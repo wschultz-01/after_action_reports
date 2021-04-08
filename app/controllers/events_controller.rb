9ca4573bce8cbdf9437d464254e51a72d7b4556f
willjschultz <willjschultz@gmail.com>
2021-04-07 22:59:50 -0400

    Stub out create in Event Controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,16 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+  
+  def create
+    @event = Event.new(title: 'title', description: 'description', date: '4/7/2021')
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+  
+  
 end
