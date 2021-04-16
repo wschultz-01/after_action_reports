e2dd6eb04503ee123060fa43eba56922b6aa76f2
willjschultz <willjschultz@gmail.com>
2021-04-15 23:26:03 -0400

    Require email uniqueness for User creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  before_save { self.email = email.downcase }
 end
