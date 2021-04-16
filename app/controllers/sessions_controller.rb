af79459dcd8c3e724120f75006c3ccbd82c90be7
willjschultz <willjschultz@gmail.com>
2021-04-16 00:30:49 -0400

    Checks for good email/password combo and renders new if no

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,14 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
+
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to user
+    else
+      flash.now[:danger] = 'Incorrect Login Details'
+      render 'new'
     end
   end
   
