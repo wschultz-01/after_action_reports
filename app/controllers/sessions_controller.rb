5b78546c1fdb5bab4423b117addf395f56872647
willjschultz <willjschultz@gmail.com>
2021-04-16 00:21:25 -0400

    Adds logic for Session Create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,11 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user
+    end
   end
   
   def destroy
